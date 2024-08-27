19529ba9f54b2889a4c06cf81ed4ce4672e691b7
Nidhish ABRAHAM <nidhishabraham@gmail.com>
2024-08-27 23:08:20 +0300

    Update main.tf
    Update main.tf

--- main.tf
+++ main.tf
@@ -7,17 +7,17 @@ terraform {
     } 
   }
   backend "azurerm" {
-resource_group_name   = "terraform-ResourceGroup"
-storage_account_name  = "heydevopsterraformtstate"
-container_name        = "tstate"
-key                   = "AZURE-KEY"
+    resource_group_name   = "terraform-ResourceGroup"
+    storage_account_name  = "heydevopsterraformtstate"
+    container_name        = "tstate"
+    key                   = "<AZURE_KEY>"
 }
   required_version = ">= 0.14.9"
 }
 
 provider "azurerm" {
   features {}
-
+  subscription_id = "<SUBSCRIPTION_ID>"
   skip_provider_registration = "true"
 }
 
@@ -29,10 +29,3 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-ResourceGroup1"
   location = "Central India"
 }
-
-
-
-
-
-
-
