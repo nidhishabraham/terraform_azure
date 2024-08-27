af5a0f17507aefe8c3e2d33ec33f4772e926505a
Nidhish ABRAHAM <nidhishabraham@gmail.com>
2024-08-27 23:27:33 +0300

    Update main.tf
    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,26 @@
 # Configure Azure Provider
 terraform {
   required_providers {
-     azurerm = {
-      source = "hashicorp/azurerm"
+    azurerm = {
+      source  = "hashicorp/azurerm"
       version = ">= 3.59.0"
-    } 
+    }
   }
+  
+  # Configure Terraform backend to store the state in Azure Storage Account
   backend "azurerm" {
     resource_group_name   = "terraform-ResourceGroup"
     storage_account_name  = "heydevopsterraformtstate"
     container_name        = "tstate"
-    key                   = "<AZURE_KEY>"
-}
+    key                   = "<AZURE_KEY>"  # Replace with the actual key name
+  }
+
   required_version = ">= 0.14.9"
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "<SUBSCRIPTION_ID>"
+  subscription_id = "<SUBSCRIPTION_ID>"  # Replace with your Azure subscription ID
   skip_provider_registration = "true"
 }
 
@@ -25,6 +28,7 @@ variable "prefix" {
   default = "terraform"
 }
 
+# Resource Group Definition
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-ResourceGroup1"
   location = "Central India"
